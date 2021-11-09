c320f7bc421b1323c4fa04b7f7885a2a0ea84c46
Alek90 <4lek.90@gmail.com>
2021-11-09 17:58:34 +0100

    Creata funzione generateEmail.

--- app.js
+++ app.js
@@ -13,8 +13,18 @@ const root = new Vue({
 
         newEmail: '', // potrei usare una funzione che prenda una nuova email dall'url, fino a generarne 10;
 
-        emails: {
+        emails: [
             // qui potrei pushare le 10 email generate;
+        ]
+    },
+
+    methods: {
+
+        generateEmail(url) {
+
+            axios.get(url).then(response => {
+                this.emails.push(response.data.response);
+            });
         }
     },
 
@@ -24,10 +34,12 @@ const root = new Vue({
             console.log(response);
             this.newEmail = response.data.response
             console.log(this.newEmail);
-        }),
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response =>{
-            console.log(response);
         })
+
+        for(let i = 0; i < 10; i++){
+            this.generateEmail('https://flynn.boolean.careers/exercises/api/random/mail')
+            console.log(this.emails);
+        }
+      
     }
 })
