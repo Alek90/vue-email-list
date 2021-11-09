248d98c532e5aed548952bdf4dacf86f4ffbb6ce
Alek90 <4lek.90@gmail.com>
2021-11-09 18:37:01 +0100

    Tutti o nessuno.
    Inserito un v-show per mandare la lista a schermo SOLO  DOPO che sono state generate tutte e 10 le email richieste. (chissà perchè: >9 true && =10 false)

--- app.js
+++ app.js
@@ -4,22 +4,31 @@
     generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
 
+// potrei usare una funzione che prenda una nuova  email dall'url, fino a generarne 10;
+
+// poi potrei pushare le 10 email generate;
+        
+
 
 const root = new Vue({
 
     el: '#root',
 
-    data: {
-
-        newEmail: '', // potrei usare una funzione che prenda una nuova email dall'url, fino a generarne 10;
+    data: {  
 
         emails: [
-            // qui potrei pushare le 10 email generate;
+            // qui verranno pushate le 10 email generate;
         ]
     },
 
     methods: {
 
+        
+        // qui la funzione "vergine" che utilizzeremo:
+
+        /** Richiediamo un indirizzo email ad un'api;
+         * @param {string} url al quale effettuare la richiesta;
+         */
         generateEmail(url) {
 
             axios.get(url).then(response => {
@@ -30,16 +39,11 @@ const root = new Vue({
 
     mounted() {
 
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response =>{
-            console.log(response);
-            this.newEmail = response.data.response
-            console.log(this.newEmail);
-        })
-
+        // richiamiamo la funzione applicando l'url desiderato al parametro:
         for(let i = 0; i < 10; i++){
-            this.generateEmail('https://flynn.boolean.careers/exercises/api/random/mail')
-            console.log(this.emails);
+            this.generateEmail('https://flynn.boolean.careers/exercises/api/random/mail');
         }
+        console.log(this.emails);
       
     }
 })
